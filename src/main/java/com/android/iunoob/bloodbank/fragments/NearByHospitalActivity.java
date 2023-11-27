e8b7bc34d23f8176dd02ce4ef9401cab2d17baac
MD NASER BIN HOSSAIN <149578308+naserPSD@users.noreply.github.com>
2023-11-27 19:55:15 +0600

    Update NearByHospitalActivity.java

--- src/main/java/com/android/iunoob/bloodbank/fragments/NearByHospitalActivity.java
+++ src/main/java/com/android/iunoob/bloodbank/fragments/NearByHospitalActivity.java
@@ -39,9 +39,9 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 /***
  Project Name: BloodBank
- Project Date: 10/22/18
- Created by: imshakil
- Email: mhshakil_ice_iu@yahoo.com
+ Project Date: 10/11/2022
+ Created by: Md Naser Bin Hossain & Reajus Salehin
+ Email: naser.hossain@northsouth.edu & reajus.shyon@northsouth.edu
  ***/
 
 public class NearByHospitalActivity extends Fragment implements
